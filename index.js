2c33eab9bc015685c77a5f005a4c170c4d9733e5
Matthew Lock <matthew_lock20@hotmail.com>
2021-01-06 15:42:01 +0000

    middleware added, index page

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path')
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Hello World of Books');
+
 })
 
 
