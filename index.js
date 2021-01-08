b89da6f8cda1e8de1c9155390c05ebf7747746ef
Matthew Lock <matthew_lock20@hotmail.com>
2021-01-08 17:15:19 +0000

    Amended front page, form now adds to array simulating database

--- index.js
+++ index.js
@@ -10,12 +10,22 @@ app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+let books = [];
 
+app.get('/', (req, res) => {
+  res.send(add)
 })
 
 app.get('/add', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/add.html')); 
+  res.sendFile(path.join(__dirname + '/public/add.html'));
+
+})
+
+app.post('/add', (req, res) => {
+  const book = req.body
+  books.push(book)
+  console.log(book)
+  res.send('Book Added!')
 })
 
 
